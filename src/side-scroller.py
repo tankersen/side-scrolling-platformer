2ad8d7b7e366273b843c30d6a9796f10efc884cc
tankersen <txa220022@utdallas.edu>
2025-11-16 17:26:16 -0600

    defined background and created for loop to define how the image tiles

--- src/side-scroller.py
+++ src/side-scroller.py
@@ -11,17 +11,48 @@ from os import listdir
 from os.path import isfile, join
 pygame.init()
 
-pygame.display.setcaption("Platformer")
+pygame.display.set_caption("Platformer")
 
 bg_color = (255,255,255)
-width, height = 1920, 1080
+x, y = 1920, 1080
 fps = 60
 player_velocity = 5
 
-window = pygame.display.set_mode((width,height))
+window = pygame.display.set_mode((x,y))
+
+def get_background(name):
+    image = pygame.image.load(join("assets","Background",name))
+    _, _, width, height = image.get_rect()
+    tiles = []
+
+    for i in range(x // width + 1):
+        for j in range(y // height + 1):
+            pos = (i * width, j * height)
+            tiles.append(pos)
+
+    return tiles, image
+
+def draw(window,background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
 
 def main(window):
     clock = pygame.time.Clock()
+    background, bg_image = get_background("Blue.png")
+
+#regulate framerate across devices
+    run = True
+    while run:
+        clock.tick(fps)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+        draw(window, background, bg_image)
 
 if __name__ == "__main__":
     main(window)
