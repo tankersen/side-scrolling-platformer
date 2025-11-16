eda2d4201baf7a41899e4f681255a2691204ee65
tankersen <txa220022@utdallas.edu>
2025-11-16 17:59:59 -0600

    created template player with  left/right movement and gravity

--- src/side-scroller.py
+++ src/side-scroller.py
@@ -14,33 +14,87 @@ pygame.init()
 pygame.display.set_caption("Platformer")
 
 bg_color = (255,255,255)
-x, y = 1920, 1080
+WIDTH, HEIGHT = 1920, 1080
 fps = 60
 player_velocity = 5
 
-window = pygame.display.set_mode((x,y))
+window = pygame.display.set_mode((WIDTH,HEIGHT))
+
+
+class Player(pygame.sprite.Sprite):
+    COLOR = (255,0,0)
+    GRAVITY = 1
+
+    def __init__(self,x,y,width,height):
+        self.rect = pygame.Rect (x,y,width,height)
+        self.x_vel = 0
+        self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+        self.fall_count = 0
+
+    def move(self, dx, dy):
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def move_left(self, vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self, vel):
+        self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+            
+    def loop(self, fps):
+        self.y_vel += min(1, (self.fall_count/ fps) * self.GRAVITY)
+        self.move(self.x_vel, self.y_vel)
+
+        self.fall_count += 1
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
+
 
 def get_background(name):
     image = pygame.image.load(join("assets","Background",name))
     _, _, width, height = image.get_rect()
     tiles = []
 
-    for i in range(x // width + 1):
-        for j in range(y // height + 1):
+    for i in range(WIDTH // width + 1):
+        for j in range(HEIGHT // height + 1):
             pos = (i * width, j * height)
             tiles.append(pos)
 
     return tiles, image
 
-def draw(window,background, bg_image):
+def draw(window,background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
 
+        player.draw(window)
+
     pygame.display.update()
+
+
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if keys[pygame.K_a]:
+        player.move_left(player_velocity)
+    if keys[pygame.K_d]:
+        player.move_right(player_velocity)
 
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
+
+    player = Player(100,100, 50, 50)
 
 #regulate framerate across devices
     run = True
@@ -52,7 +106,10 @@ def main(window):
                 run = False
                 break
 
-        draw(window, background, bg_image)
+        player.loop(fps)
+        handle_move(player)
+
+        draw(window, background, bg_image, player)
 
 if __name__ == "__main__":
     main(window)
