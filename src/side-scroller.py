70ff076f086f2858b60efe2c0b083dcfe6e9cfa1
tankersen <txa220022@utdallas.edu>
2025-11-24 15:41:15 -0600

    defined landing and hitting head

--- src/side-scroller.py
+++ src/side-scroller.py
@@ -97,6 +97,15 @@ class Player(pygame.sprite.Sprite):
         self.fall_count += 1
         self.update_sprite()
 
+    def landed(self):
+        self.fall_count = 0
+        self.y_vel = 0
+        self.jump_count = 0
+
+    def hit_head(self):
+        self.count = 0
+        self.y_vel *= -1
+
     def update_sprite(self):
         sprite_sheet = "idle"
         if self.x_vel != 0:
@@ -164,7 +173,25 @@ def draw(window,background, bg_image, player, objects):
     pygame.display.update()
 
 
-def handle_move(player):
+
+
+def handle_vertical_collision(player, objects, dy):
+    collided_objects = []
+    for obj in objects:
+        if pygame.sprite.collide_mask(player, obj):
+            if dy > 0:
+                player.rect.bottom = obj.rect.top
+                player.landed()
+            elif dy < 0:
+                player.rect.top = obj.rect.bottom
+                player.hit_head
+
+        collided_objects.append(obj)
+
+    return collided_objects
+
+
+def handle_move(player, objects):
     keys = pygame.key.get_pressed()
 
     player.x_vel = 0
