009e3829e7540be7130d56f433f6f792318a4c66
tankersen <txa220022@utdallas.edu>
2025-11-24 20:40:28 -0600

    implemented side scrolling

--- src/side-scroller.py
+++ src/side-scroller.py
@@ -73,6 +73,15 @@ class Player(pygame.sprite.Sprite):
         self.direction = "left"
         self.animation_count = 0
         self.fall_count = 0
+        self.jump_count = 0
+
+    def jump(self):
+        self.y_vel = -self.GRAVITY * 8
+        self.animation_count = 0
+        self.jump_count += 1
+        if self.jump_count == 1:
+            self.count = 0
+        
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -91,7 +100,7 @@ class Player(pygame.sprite.Sprite):
             self.animation_count = 0
             
     def loop(self, fps):
-       # self.y_vel += min(1, (self.fall_count/ fps) * self.GRAVITY)
+        self.y_vel += min(1, (self.fall_count/ fps) * self.GRAVITY)
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
@@ -108,7 +117,16 @@ class Player(pygame.sprite.Sprite):
 
     def update_sprite(self):
         sprite_sheet = "idle"
-        if self.x_vel != 0:
+        if self.y_vel != 0:
+            if self.jump_count == 1:
+                sprite_sheet = "jump"
+            elif self.jump_count == 2:
+                sprite_sheet = "double_jump"
+                
+        elif self.y_vel > self.GRAVITY * 2:
+            sprite_sheet = "fall"
+
+        elif self.x_vel != 0:
             sprite_sheet = "run"
 
         sprite_sheet_name = sprite_sheet + "_" + self.direction
@@ -123,8 +141,8 @@ class Player(pygame.sprite.Sprite):
         self.mask = pygame.mask.from_surface(self.sprite)
 
 
-    def draw(self, win):
-        win.blit(self.sprite, (self.rect.x, self.rect.y))
+    def draw(self, win, offset_x):
+        win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
 
 
 class Object(pygame.sprite.Sprite):
@@ -136,8 +154,8 @@ class Object(pygame.sprite.Sprite):
         self.height = height
         self.name = name
 
-    def draw(self,win):
-        win.blit(self.image, (self.rect.x,self.rect.y))
+    def draw(self,win, offset_x):
+        win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
 
 
 class Block(Object):
@@ -161,18 +179,16 @@ def get_background(name):
 
     return tiles, image
 
-def draw(window,background, bg_image, player, objects):
+def draw(window,background, bg_image, player, objects, offset_x):
     for tile in background:
         window.blit(bg_image, tile)
 
     for obj in objects:
-        obj.draw(window)
+        obj.draw(window, offset_x)
 
-        player.draw(window)
+    player.draw(window, offset_x)
 
     pygame.display.update()
-
-
 
 
 def handle_vertical_collision(player, objects, dy):
@@ -200,6 +216,8 @@ def handle_move(player, objects):
     if keys[pygame.K_d]:
         player.move_right(player_velocity)
 
+    handle_vertical_collision(player, objects, player.y_vel)
+
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
@@ -208,6 +226,9 @@ def main(window):
 
     player = Player(100,100, 50, 50)
     floor = [Block(i*block_size, HEIGHT - block_size, block_size) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size)]
+
+    offset_x = 0
+    scroll_area_width = 200
 
 
 #regulate framerate across devices
@@ -220,10 +241,18 @@ def main(window):
                 run = False
                 break
 
-        player.loop(fps)
-        handle_move(player)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and player.jump_count < 2:
+                    player.jump()
 
-        draw(window, background, bg_image, player, floor)
+        player.loop(fps)
+        handle_move(player, floor)
+
+        draw(window, background, bg_image, player, floor, offset_x)
+
+        if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or(
+                (player.rect.left - offset_x <= scroll_area_width) and player.x_vel<0):
+            offset_x += player.x_vel
 
 if __name__ == "__main__":
     main(window)
